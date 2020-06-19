38e66660d92862c8ed0a5fbbf38b9d58104f1e58
Warui Ngugi <waruingugientp@gmail.com>
2020-06-19 09:51:45 +0300

    Remove tilt js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,6 @@
 
 var options = {
-  strings: ['Since long ago I have always..', 'Since my days as a child, I have always love reading and writing. So far, l have read hundreds of books, mostly non-fiction and self- improvement genres. I have written extensively too, both short articles and whole books. Here are previews of some of my books. Lately, l have become a composer of poems too.'],
+  strings: ['Since long ago I have always..', 'Since my days as a child, I have always loved reading and writing. So far, l have read hundreds of books, mostly non-fiction and self-improvement genres. I have written extensively too, both short articles and whole books. Here are previews of some of my books. Lately, l have become a composer of poems too.'],
   typeSpeed: 40
 };
 
